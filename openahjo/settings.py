91981d68813f8438f032fb8103f021045bb6aeaa
Juha Yrjölä <juha.yrjola@iki.fi>
2013-05-12 23:44:12 +0300

    Custom tastypie-swagger template

--- openahjo/settings.py
+++ openahjo/settings.py
@@ -114,9 +114,7 @@ ROOT_URLCONF = 'openahjo.urls'
 WSGI_APPLICATION = 'openahjo.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates')
 )
 
 INSTALLED_APPS = (
