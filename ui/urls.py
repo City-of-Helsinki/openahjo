44922ec186b55361eb6089166af03aaeedfd4672
Juha Yrjölä <juha.yrjola@iki.fi>
2013-10-23 19:07:01 +0300

    Use Finnish URLs by default, add redirects from old URLs

--- ui/urls.py
+++ ui/urls.py
@@ -1,11 +1,19 @@
 from django.conf.urls import patterns
+from django.views.generic.base import RedirectView
+from django.conf import settings
+
+prefix = getattr(settings, 'URL_PREFIX', '')
 
 urlpatterns = patterns('',
     (r'^$', 'ui.views.home_view'),
-    (r'^issue/$', 'ui.views.issue_list'),
-    (r'^issue/map/$', 'ui.views.issue_list_map'),
-    (r'^issue/(?P<slug>[\w-]+)/((?P<pm_slug>[\w-]+)-(?P<year>\d+)-(?P<number>\d+)/)?$', 'ui.views.issue_details'),
-    (r'^policymaker/$', 'ui.views.policymaker_list'),
-    (r'^policymaker/(?P<slug>[\w-]+)/((?P<year>\d+)/(?P<number>\d+)/)?$', 'ui.views.policymaker_details'),
-    (r'^about/$', 'ui.views.about'),
+    (r'^asia/$', 'ui.views.issue_list'),
+    (r'^asia/kartta/$', 'ui.views.issue_list_map'),
+    (r'^asia/(?P<slug>[\w-]+)/((?P<pm_slug>[\w-]+)-(?P<year>\d+)-(?P<number>\d+)/)?$', 'ui.views.issue_details'),
+    (r'^paattaja/$', 'ui.views.policymaker_list'),
+    (r'^paattaja/(?P<slug>[\w-]+)/((?P<year>\d+)/(?P<number>\d+)/)?$', 'ui.views.policymaker_details'),
+    (r'^tietoa/$', 'ui.views.about'),
+
+    (r'^issue(?P<rest>.*)$', RedirectView.as_view(url='/' + prefix + 'asia%(rest)s')),
+    (r'^policymaker(?P<rest>.*)$', RedirectView.as_view(url='/' + prefix + 'paattaja%(rest)s')),
+    (r'^about(?P<rest>.*)$', RedirectView.as_view(url='/' + prefix + 'tietoa%(rest)s')),
 )
