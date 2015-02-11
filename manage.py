d8f87e0ef95450c2aa2230690c044fb5a5c4e46f
Juha Yrjölä <juha.yrjola@iki.fi>
2015-02-11 16:57:55 +0200

    Add Raven for better exception logging

--- manage.py
+++ manage.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 import os
 import sys
+import traceback
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openahjo.settings")
 
     from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+    try:
+        execute_from_command_line(sys.argv)
+    except Exception as e:
+        if sys.stdout.isatty():
+            traceback.print_exc()
+        else:
+            from django.conf import settings
+            if settings.DEBUG or not hasattr(settings, 'RAVEN_CONFIG'):
+                raise
+            from raven.contrib.django.models import get_client
+            exc_info = sys.exc_info()
+            if getattr(exc_info[0], 'skip_sentry', False):
+                raise
+            get_client().captureException(exc_info)
--- openahjo/settings.py
+++ openahjo/settings.py
@@ -140,10 +140,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'raven.contrib.django.raven_compat',
     'tastypie_swagger',
     'corsheaders',
     'popolo',
