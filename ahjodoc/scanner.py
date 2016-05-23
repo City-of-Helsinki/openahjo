d8e0f6838bc40818bf86a60f2f98cd453271217c
Juha Yrjölä <juha.yrjola@iki.fi>
2016-05-23 12:32:56 +0300

    Add URL to skip list

--- ahjodoc/scanner.py
+++ ahjodoc/scanner.py
@@ -39,6 +39,7 @@ SKIP_URL_LIST = [
     'http://openhelsinki.hel.fi/files/Rakennusvirasto_52000/Tulosryhman%20johtaja_521112VH1/HKR%202014-12-16%20521112VH1%2012%20Pk%20Su.zip', # wrong date
     'http://openhelsinki.hel.fi/files/Sosiaali-%20ja%20terveyslautakunta_81000/Sote%202013-06-04%20Sotelk%209%20Pk%20Su.zip', # missing attachment
     'http://openhelsinki.hel.fi/files/Sosiaali-%20ja%20terveyslautakunta_81000/Sote%202013-06-04%20Sotelk%209%20El%20Su.zip', # missing attachment
+    'http://openhelsinki.hel.fi/files/Asuntotuotantotoimisto_60201/Toimitusjohtaja_60201VH1/Att%202016-05-16%2060201VH1%20146%20Pk%20Su.zip', # empty ZIP
 ]
 
 CHUNK_SIZE = 32*1024
